e58b9f994067d6e942326f2245dee41762e3efeb
Ayelen Guini <ayelenguini@Ayelens-MacBook-Pro.local>
2019-03-15 13:06:32 -0300

    finished

---
+++ detox/App.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import { Counter } from './Counter';
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super();
+    this.state = { buttonPressed: 0 };
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+  }
+  handleButtonPress() {
+    this.setState({ buttonPressed: this.state.buttonPressed + 1 });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text testID={"welcome"} style={styles.welcome}>Welcome to React Native!</Text>
+        <View testID={'counterContainer'}>
+          <Counter value={this.state.buttonPressed} testID={"runningState"} style={styles.instructions} />
+          <Button onPress={this.handleButtonPress} title={"Start"} testID={"startButton"} />
+        </View>
+        <TextInput testID={"input"} style={styles.input} />
+        <View style={styles.listContainer}>
+          <FlatList testID={"list"} data={["Ayelen", "Matias", "Cecilia", "Marcelo"]} renderItem={({ item }) => <Text style={styles.listItem}>{item}</Text>} />
+        </View>
+        <Button onPress={this.handleButtonPress} title={"Start"} testID={"startButton"} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  counterContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderWidth: 1,
+    padding: 10
+  },
+  listContainer: {
+    height: 100,
+    width: "80%",
+    backgroundColor: "#eee",
+  },
+  listItem: {
+    height: 100,
+  }
+});
